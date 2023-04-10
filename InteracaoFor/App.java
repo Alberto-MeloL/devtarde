fc3fef6a702abc502e00ebc850d2249a578af414
alberto12345678900 <albertomeloteco@gmail.com>
2023-04-10 16:53:17 -0300

    10/03/2023

---
+++ InteracaoFor/App.java
@@ -0,0 +1,8 @@
+package InteracaoFor;
+
+public class App {
+    public static void main(String[] args) {
+        ExemploFor obj = new ExemploFor();
+        obj.Exemplo1();
+    }
+}
---
+++ InteracaoFor/ExemploFor.java
@@ -0,0 +1,15 @@
+package InteracaoFor;
+
+import java.util.Scanner;
+
+public class ExemploFor {
+    Scanner sc = new Scanner(System.in);
+
+    public void Exemplo1() {
+int i= 0;
+        for (i = 0; i < 100; i++){ 
+        System.out.println("O nº da Interacao é "+i);
+    }
+}
+
+}
